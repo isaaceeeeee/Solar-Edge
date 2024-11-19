18db965c5888e5ce263b6de8779b9d432a55f518
Isaac Edward Ellard <104221346+isaaceeeeee@users.noreply.github.com>
2024-11-19 13:44:55 +0000

    Update sites.py

--- sites.py
+++ sites.py
@@ -1,4 +1,5 @@
-# solaredge.py
+# sites.py
+# Creates a list of all sites registered under your account
 import requests
 import pandas as pd
 import json
@@ -23,3 +24,50 @@ function = response.json()
 print(json.dumps(function, indent=4))
 with open("output.json", "w") as json_file:
     json.dump(function, json_file, indent=4)
+
+'''
+Example output (JSON):
+{
+    "sites": {
+        "count": 1,
+        "site": [
+            {
+                "id": 1,
+                "name": "Site Name",
+                "accountId": 1,
+                "status": "Active",
+                "peakPower": 100.0,
+                "lastUpdateTime": "2024-01-01",
+                "installationDate": "2024-01-01",
+                "ptoDate": null,
+                "notes": "",
+                "type": "Optimizers & Inverters",
+                "location": {
+                    "country": "United Kingdom",
+                    "city": "City",
+                    "address": "Address",
+                    "address2": "",
+                    "zip": "Post Code",
+                    "timeZone": "Europe/London",
+                    "countryCode": "GB"
+                },
+                "alertQuantity": 1,
+                "highestImpact": 2,
+                "primaryModule": {
+                    "manufacturerName": "Manufacturer",
+                    "modelName": "Model",
+                    "maximumPower": 100.0
+                },
+                "uris": {
+                    "DETAILS": "/site/1/details",
+                    "DATA_PERIOD": "/site/1/dataPeriod",
+                    "OVERVIEW": "/site/1/overview"
+                },
+                "publicSettings": {
+                    "isPublic": false
+                }
+            },
+        ]
+    }
+}
+'''
